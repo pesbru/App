c03c4006090fc844f4a76a0fed107aa992d201e7
pesbru <pesbru@gmail.com>
2026-07-12 00:50:04 +0200

    Update CL_M_Multiplos.js

--- HTML/Grupos_Baterias/CL_M_Multiplos.js
+++ HTML/Grupos_Baterias/CL_M_Multiplos.js
@@ -10,14 +10,14 @@ var CONFIG_EXAMENES = {
         },
         {
             id: "LG_002",
-            titulo: "Selecciona lo múltiplos de 3",
-            archivo: "CL_M_Múltiplos3.js", // ¡CORREGIDO! Añadido el .js al final
+            titulo: "Selecciona los múltiplos de 3",
+            archivo: "CL_M_Multiplos3.js", // ¡CORREGIDO! Añadido el .js al final
             ruta: "Baterias" 
         },
         {
             id: "LG_003",
-            titulo: "Selecciona lo múltiplos de 5",
-            archivo: "CL_M_Múltiplos5.js", // ¡CORREGIDO! Añadido el .js al final
+            titulo: "Selecciona los múltiplos de 5",
+            archivo: "CL_M_Multiplos5.js", // ¡CORREGIDO! Añadido el .js al final
             ruta: "Baterias" 
         }
     ]
