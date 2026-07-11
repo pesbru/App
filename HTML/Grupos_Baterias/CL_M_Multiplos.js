3804bc7251696451a1e165d88676cbea80699ef4
pesbru <pesbru@gmail.com>
2026-07-11 15:32:09 +0200

    Add files via upload

---
+++ HTML/Grupos_Baterias/CL_M_Multiplos.js
@@ -0,0 +1,12 @@
+
+var CONFIG_EXAMENES = {
+    asignatura: "Matemáticas", // Corregido el nombre de la asignatura
+    baterias: [
+        {
+            id: "LG_001",
+            titulo: "Clasifica números: pares / impares",
+            archivo: "CL_M_Pares-Impares.js", // ¡CORREGIDO! Añadido el .js al final
+            ruta: "Baterias" 
+        }
+    ]
+};
