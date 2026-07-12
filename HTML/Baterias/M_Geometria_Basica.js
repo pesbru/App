578e3c02d83a6204a21b2c880e2172e16ddcde38
pesbru <pesbru@gmail.com>
2026-07-12 20:12:06 +0200

    Add files via upload

---
+++ HTML/Baterias/M_Geometria_Basica.js
@@ -0,0 +1,26 @@
+var DATOS_BATERIA = [
+
+ { id: 1, enunciado: "Circulo.png", opciones: ["Círculo", "Circunferencia", "Pentágono", "Hexágono"], correcta:  0, observaciones: "Es una superficie" },
+ { id: 2, enunciado: "Circunferencia.png", opciones: ["Círculo", "Circunferencia", "Cuadrado", "Octógono"], correcta:  1, observaciones: "Es línea" },
+ { id: 3, enunciado: "Cuadrado.png", opciones: ["Pentágono", "Círculo", "Cuadrado", "Octógono"], correcta:  2, observaciones: "Cuenta los lados" },
+ { id: 4, enunciado: "Heptagono.png", opciones: ["Triángulo escaleno", "Hexágono", "Pentágono", "Heptágono"], correcta:  3, observaciones: "Cuenta los lados" },
+ { id: 5, enunciado: "Hexagono.png", opciones: ["Hexágono", "Pentágono", "Triángulo equilátero", "Heptágono"], correcta:  0, observaciones: "Cuenta los lados" },
+ { id: 6, enunciado: "Octogono.png", opciones: ["Heptágono", "Octógono", "Pentágono", "Hexágono"], correcta:  1, observaciones: "Cuenta los lados" },
+ { id: 7, enunciado: "Pentagono.png", opciones: ["Heptágono", "Octógono", "Pentágono", "Círculo"], correcta:  2, observaciones: "Cuenta los lados" },
+ { id: 8, enunciado: "T_Equilatero.png", opciones: ["Triángulo escaleno", "Triángulo rectánguno", "Triángulo isósceles", "Triángulo equilátero"], correcta:  3, observaciones: "Observa los lados" },
+ { id: 9, enunciado: "T_Equilatero2.png", opciones: ["Triángulo equilátero", "Triángulo escaleno", "Triángulo rectánguno", "Triángulo isósceles"], correcta:  0, observaciones: "Observa los lados" },
+ { id: 10, enunciado: "T_Escaleno.png", opciones: ["Triángulo equilátero", "Triángulo escaleno", "Triángulo rectánguno", "Triángulo isósceles"], correcta:  1, observaciones: "Observa los lados" },
+ { id: 11, enunciado: "T_Isosceles.png", opciones: ["Triángulo equilátero", "Triángulo obtusángulo", "Triángulo isósceles", "Triángulo rectánguno"], correcta:  2, observaciones: "Observa los lados" },
+ { id: 12, enunciado: "T_Obtusangulo.png", opciones: ["Triángulo equilátero", "Triángulo rectánguno", "Triángulo isósceles", "Triángulo obtusángulo"], correcta:  3, observaciones: "Observa los ángulos" },
+ { id: 13, enunciado: "T_Rectangulo.png", opciones: ["Triángulo obtusángulo", "Triángulo rectángulo", "Triángulo equilátero", "Triángulo isósceles"], correcta:  1, observaciones: "Observa los ángulos" },
+ { id: 14, enunciado: "T_Rectangulo2.png", opciones: ["Triángulo equilátero", "Triángulo obtusángulo", "Triángulo rectánguno", "Triángulo isósceles"], correcta:  2, observaciones: "Observa los ángulos" },
+ { id: 15, enunciado: "T_Escaleno.png", opciones: ["Triángulo equilátero", "Triángulo obtusángulo", "Triángulo rectánguno", "Triángulo isósceles"], correcta:  1, observaciones: "Observa los ángulos" },
+ { id: 16, enunciado: "Angulo_Agudo.png", opciones: ["Ángulo obtuso", "Ángulo agudo", "Ángulo llano", "Ángulo recto"], correcta:  1, observaciones: "" },
+ { id: 17, enunciado: "Angulo_Recto.png", opciones: ["Ángulo obtuso", "Ángulo agudo", "Ángulo llano", "Ángulo recto"], correcta:  3, observaciones: "" },
+ { id: 18, enunciado: "Angulo_Obtuso.png", opciones: ["Ángulo obtuso", "Ángulo agudo", "Ángulo llano", "Ángulo recto"], correcta:  0, observaciones: "" },
+ { id: 19, enunciado: "T_Rectangulo.png", opciones: ["Triángulo escaleno", "Triángulo equilátero", "Triángulo isósceles", "Triángulo obtusángulo"], correcta:  0, observaciones: "Observa los lados" },
+ { id: 20, enunciado: "Angulo_Llano.png", opciones: ["Ángulo obtuso", "Ángulo agudo", "Ángulo llano", "Ángulo recto"], correcta:  2, observaciones: "" }
+
+
+
+];
