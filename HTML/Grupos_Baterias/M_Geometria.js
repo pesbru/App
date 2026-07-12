c8b49e0b6154250941c3c37f5d5befaf0b67500a
pesbru <pesbru@gmail.com>
2026-07-12 20:25:23 +0200

    Add files via upload

---
+++ HTML/Grupos_Baterias/M_Geometria.js
@@ -0,0 +1,13 @@
+
+
+var CONFIG_EXAMENES = {
+    asignatura: "Matemáticas", // Corregido el nombre de la asignatura
+    baterias: [
+        {
+            id: "Probl_01",
+            titulo: "Geometría básica",
+            archivo: "M_Geometria_Basica.js", // ¡CORREGIDO! Añadido el .js al final
+            ruta: "Baterias" 
+        }
+    ]
+};
